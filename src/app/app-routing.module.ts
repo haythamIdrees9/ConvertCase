56f182ca4c621099d7ec9e94179a1b648fbae874
haythamIdrees9 <haythamidrees9@gmail.com>
2023-08-26 13:07:20 +0300

    code structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from "@angular/core";
 import { PreloadAllModules, Route, RouterModule } from "@angular/router";
 
 const routes: Route[] = [
-  {path:'text-case-tools',loadChildren: () => import('./text-converter/text-converter.module').then(m => m.TextConverterModule)},
-  {path:'url-encode-decode',loadChildren:() => import('./encode-decode-url/url-encode-decode.module').then(m => m.URLEncodeDecodeModule) },
-  {path:'text-manipulation',loadChildren:() => import('./text-manipulation/text-manipulation.module').then(m => m.TextManipulationModule) },
+  {path:'text-case-tools',loadChildren: () => import('./components/text-converter/text-converter.module').then(m => m.TextConverterModule)},
+  {path:'url-encode-decode',loadChildren:() => import('./components/encode-decode-url/url-encode-decode.module').then(m => m.URLEncodeDecodeModule) },
+  {path:'text-manipulation',loadChildren:() => import('./components/text-manipulation/text-manipulation.module').then(m => m.TextManipulationModule) },
   {path:'**', redirectTo:'text-case-tools',pathMatch:'full'},
 ]
 @NgModule({
