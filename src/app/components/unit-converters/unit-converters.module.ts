05f98824d075a04d52da69b08c8b5df50aa2e429
haythamIdrees9 <haythamidrees9@gmail.com>
2023-08-27 19:45:25 +0300

    make some fixes

--- src/app/components/unit-converters/unit-converters.module.ts
+++ src/app/components/unit-converters/unit-converters.module.ts
@@ -4,16 +4,19 @@ import { RouterModule } from '@angular/router';
 import { ResultActionsBtnComponent } from '../../UI/result-actions-btn/result-actions-btn.component';
 import { UnitConvertersComponent } from './unit-converters.component';
 import { ExpandablePanalModule } from 'src/app/UI/expandable-panal/expandable-panal.module';
+import { MainComponent } from './main/main.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
-  declarations: [UnitConvertersComponent],
+  declarations: [UnitConvertersComponent, MainComponent],
   imports: [
       FormsModule,
       ResultActionsBtnComponent,
       ExpandablePanalModule,
+      CommonModule,
        RouterModule.forChild([
         { path: '', component: UnitConvertersComponent,children:[
-
+          {path:'',component:MainComponent},
           {path: 'length', loadChildren: () => import('./length-converter/length-converter.module').then(m => m.LengthConverterModule)}]
         }
       ])
