e503d40edb06602c88024577ce4e789f8ac6ea73
haythamIdrees9 <haythamidrees9@gmail.com>
2023-09-09 01:56:52 +0300

    hot fix for replace url

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,9 @@ import { Route, RouterModule } from "@angular/router";
 
 const routes: Route[] = [
   // redirect
-  {path:'text-case-tools', redirectTo:'text-tools/text-case-converter',pathMatch:'full'},
-  {path:'text-manipulation' ,redirectTo:'text-tools/text-manipulation',pathMatch:'full'},
-  {path:'encode-decode' ,redirectTo:'programming-tools/encode-decode',pathMatch:'full'},
+  {path:'text-case-converter', redirectTo:'text-tools/text-case-converter'},
+  {path:'text-manipulation' ,redirectTo:'text-tools/text-manipulation'},
+  {path:'encode-decode' ,redirectTo:'programming-tools/encode-decode'},
  
   {path:'text-tools',loadChildren: () => import('./components/text-tools/text-tools.module').then(m => m.TextToolsModule)},
   {path:'programming-tools',loadChildren:() => import('./components/programming-tools/programming-tools.module').then(m => m.ProgrammingToolsModule) },
