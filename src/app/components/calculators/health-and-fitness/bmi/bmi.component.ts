f87a8e2e057f3a054bc4fbdd1aba823548cdd850
haythamIdrees9 <haythamidrees9@gmail.com>
2023-09-11 17:05:55 +0300

    basic bmi ready, need content and blogs

--- src/app/components/calculators/health-and-fitness/bmi/bmi.component.ts
+++ src/app/components/calculators/health-and-fitness/bmi/bmi.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { minorWords } from '../../../../utils/words';
-import { ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MetaService } from '../../../services/meta.service';
 import { InfoService } from './info.service';
 
@@ -8,28 +7,71 @@ import { InfoService } from './info.service';
 @Component({
   selector: 'app-bmi',
   templateUrl: './bmi.component.html',
-  styleUrls: ['./bmi.component.scss','../../../container.scss'],
-  providers:[InfoService]
+  styleUrls: ['./bmi.component.scss', '../../../container.scss'],
+  providers: [InfoService]
 })
 export class BMIComponent implements OnInit {
-
-  constructor(private route:ActivatedRoute, private metaService: MetaService, private infoService:InfoService) { }
+  avgWeight = '50';
+  avgWeightPound = '110';
+  avgHeightFeet = '5';
+  avgHeightInch = '7';
+  avgHeight = '170';
+  bmiForm: FormGroup;
+  bmiResult: { bmi: number, category: string,status:string } = {bmi:16,category:'',status:''};
+  isUSType = true;
+  constructor(private metaService: MetaService, private formBuilder: FormBuilder) {
+    // Initialize the form group with validators
+    this.bmiForm = this.formBuilder.group({
+      height: [this.avgHeight, [Validators.required, Validators.min(1)]], 
+      heightFeet: [this.avgHeightFeet, [Validators.required, Validators.min(1)]], 
+      heightInch: [this.avgHeightInch, [Validators.required, Validators.min(0)]], 
+      weight: [this.avgWeight, [Validators.required, Validators.min(1)]],
+      weightPound: [this.avgWeightPound, [Validators.required, Validators.min(1)]],
+      
+    });
+  }
 
   ngOnInit(): void {
-    this.handleSeo()
+    this.handleSeo();
+    this.calculateBMI_US();
   }
-  
+
+  setUnitType(isUSType:boolean){
+    this.isUSType = isUSType;
+  }
+
   private handleSeo() {
     this.metaService.setTitle('Text Conversion: Uppercase, Lowercase, Title and More');
     this.metaService.setDescription(`Text case converter - A set of functions that can be used to convert text to different cases, such as uppercase, lowercase, title case, and camel case. Perfect for formatting text for different purposes, such as headings, titles, and code.`);
     this.metaService.setKeywords("uppercase conversion, lowercase conversion, title case conversion, capitalized case conversion, camel case conversion, kebab case conversion, snake case conversion, inverse case conversion")
   }
 
+  calculateBMI_US(){
+    this.feetAndInchesToMeters();
+    this.poundsToKilograms();
+    this.calculateBMI();
+  }
 
-  calculateBMI(weightInKg:number, heightInM:number) {
+  feetAndInchesToMeters() {
+    const feetInMeters = Number(this.avgHeightFeet) * 30.48;
+    const inchesInMeters = Number(this.avgHeightInch) * 2.54;
+    this.avgHeight = `${parseFloat((feetInMeters + inchesInMeters).toFixed(2))}`;
+    this.bmiForm.get('height')?.setValue(this.avgHeight)
+  }
+
+  poundsToKilograms() {
+    this.avgWeight = `${parseFloat((Number(this.avgWeightPound) * 0.453592).toFixed(2))}`;
+    this.bmiForm.get('weight')?.setValue(this.avgWeight)
+  }
+
+
+  calculateBMI() {
+    const heightInM = (this.bmiForm.get('height')?.value / 100);
+    const weightInKg = this.bmiForm.get('weight')?.value;
+
     // Check for valid input
     if (weightInKg <= 0 || heightInM <= 0) {
-        return "Invalid input. Please enter valid weight and height.";
+      return;
     }
 
     // Calculate BMI
@@ -37,22 +79,77 @@ export class BMIComponent implements OnInit {
 
     // Determine BMI category
     let category;
-    if (bmi < 18.5) {
-        category = "Underweight";
-    } else if (bmi >= 18.5 && bmi < 24.9) {
-        category = "Normal Weight";
-    } else if (bmi >= 25 && bmi < 29.9) {
-        category = "Overweight";
-    } else {
-        category = "Obese";
+    if (bmi < 16) {
+      category = "Underweight";
+    } else if (bmi < 17) {
+      category = "Moderate thinness";
+    } else if (bmi < 18.5) {
+      category = "Mild Thinness";
+    } else if (bmi >= 18.5 && bmi < 25) {
+      category = "Normal Weight";
+    } else if (bmi >= 25 && bmi < 30) {
+      category = "overweight";
+    } else if (bmi >= 30 && bmi < 35) {
+      category = "obese class 1";
+    } else if (bmi >= 35 && bmi < 40) {
+      category = "obese class 2";
+    }else {
+      category = "obese class 3";
     }
 
-    return {
-        bmi: bmi.toFixed(2), // Limit to two decimal places
-        category: category,
+    this.bmiResult = {
+      bmi: parseFloat(bmi.toFixed(1)), // Limit to two decimal places
+      category: category,
+      status: category.toLowerCase().replace(/\s/g,'-')
     };
-}
+  }
 
 
 
 }
+
+
+/**
+ * 
+ * let ctx = document.getElementById("split-chart")
+
+let a = 300 // fixed width for the chart
+
+new Chart(ctx, {
+  type: "doughnut",
+  labels: ["yellow","green","Red", "pink", "blue","Invisible"],
+  width: a,
+  height: a,
+  options: {
+    cutout: "70%",
+    rotation: -90,
+    clip: { left: 0, top: 0, right: 0, bottom: -1 * 0.5 * a },
+    plugins: {
+      legend: {
+        display: false
+      }
+    }
+  },
+  data: {
+    datasets: [
+      {
+        label: "%",
+        data: [10, 2, 3,13,10,10,10,8,65],
+        backgroundColor: [
+      "#BC2020","#D38888","#FFE400", "#008137", "#FFE400","#D38888","#BC2020","#8A0101",
+          "rgba(46, 204, 113, 0)"
+        ],
+          borderColor: [
+"#BC2020","#D38888","#FFE400", "#008137", "#FFE400","#D38888","#BC2020","#8A0101",
+          "rgba(255, 255, 255 ,0)"
+        ],
+
+        
+        borderWidth: 1
+      }
+    ]
+  }
+});
+
+ * 
+ */
--- src/app/components/calculators/health-and-fitness/bmi/bmi.module.ts
+++ src/app/components/calculators/health-and-fitness/bmi/bmi.module.ts
@@ -1,17 +1,18 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { sharedModule } from '../../../../UI/shared.module';
-import { BMIComponent } from './bmi.component';
-import { ResultActionsBtnComponent } from '../../../../UI/result-actions-btn/result-actions-btn.component';
 import { LinksContainerComponent } from '../../links-container/links-container.component';
+import { BMIComponent } from './bmi.component';
+import { HighchartsChartModule } from 'highcharts-angular';
 
 @NgModule({
   declarations: [BMIComponent],
   imports: [sharedModule,
     FormsModule,
+    ReactiveFormsModule,
     LinksContainerComponent,
-    ResultActionsBtnComponent,
+    HighchartsChartModule,
     RouterModule.forChild([{ path: '', component: BMIComponent }])]
 
 })
