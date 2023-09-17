7f4f0c836bec2b36e649bef4014d6dc1541dd166
haythamIdrees9 <haythamidrees9@gmail.com>
2023-09-17 19:43:26 +0300

    implement homeowrk out blog

--- src/app/blogs/health/home-workouts-beginners-guide/home-workouts-beginners-guide.component.ts
+++ src/app/blogs/health/home-workouts-beginners-guide/home-workouts-beginners-guide.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MetaService } from 'src/app/components/services/meta.service';
 
 @Component({
   selector: 'app-home-workouts-beginners-guide',
   templateUrl: './home-workouts-beginners-guide.component.html',
   styleUrls: ['./home-workouts-beginners-guide.component.scss'],
 })
-export class HomeWorkoutsBeginnersGuideComponent {
+export class HomeWorkoutsBeginnersGuideComponent implements OnInit {
+
+  constructor(private metaService: MetaService) {
+
+  }
+  ngOnInit(): void {
+    this.metaService.setKeywords("home workouts, beginner's fitness, setting goals, fitness journey, workout routine, fitness goals, workout space, bodyweight exercises, nutrition tips, healthy lifestyle, motivation, fitness beginners, starting fitness, exercise at home")
+    this.metaService.setDescription("Discover how to kickstart your fitness journey at home with this beginner's guide. Learn to set realistic goals, create a workout space, and choose the right exercises. Get motivated and embark on a path to a healthier lifestyle today.")
+    this.metaService.setTitle("Beginner's Guide to Home Workouts");
+  }
 
 }
